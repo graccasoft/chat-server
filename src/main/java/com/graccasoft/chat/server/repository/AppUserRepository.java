4ce976c5069f50cca2e4a24984cf0909ad5fdf6d
Gracious Mashasha <graccasoft263@gmail.com>
2024-05-13 11:13:00 +0200

    User details service and repository

---
+++ src/main/java/com/graccasoft/chat/server/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.graccasoft.chat.server.repository;
+
+import com.graccasoft.chat.server.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<UserDetails> findByUsername(String username);
+}
---
+++ src/main/java/com/graccasoft/chat/server/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.graccasoft.chat.server.service;
+
+import com.graccasoft.chat.server.repository.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public UserDetailsServiceImpl(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return appUserRepository.findByUsername(username)
+                .orElseThrow( ()-> new UsernameNotFoundException("User with name not found") );
+    }
+}
