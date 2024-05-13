3d47c96076ba6b5653342e869e0370e3156d76af
Gracious Mashasha <graccasoft263@gmail.com>
2024-05-13 11:34:43 +0200

    Chat rest controller impl

--- src/main/java/com/graccasoft/chat/server/controller/ChatController.java
+++ src/main/java/com/graccasoft/chat/server/controller/ChatController.java
@@ -1,23 +1,42 @@
 package com.graccasoft.chat.server.controller;
 
 import com.graccasoft.chat.server.model.ChatMessage;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.graccasoft.chat.server.model.SendMessageDto;
+import com.graccasoft.chat.server.service.ChatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+
 @RestController
-@RequestMapping("chat-room")
+@RequestMapping("v1/chat-room")
 public class ChatController {
 
-    List<ChatMessage> getRoomMessages(){
-        return null;
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
     }
 
-    ChatMessage sendMessage(){
-        return null;
+    @GetMapping("{roomId}")
+    List<ChatMessage> getRoomMessages(@PathVariable Long roomId) {
+        return chatService.getRoomMessages(roomId);
     }
 
-    void deleteMessage(){
+    @PostMapping("{roomId}")
+    ResponseEntity<ChatMessage> sendMessage(
+            @PathVariable Long roomId,
+            @RequestBody SendMessageDto sendMessageDto,
+            Authentication authentication) {
+        ChatMessage message = chatService.sendMessage(sendMessageDto, authentication.getName());
+        return ResponseEntity.created(URI.create("v1//chat-room/" + roomId + "/message" + message.getId()))
+                .body(message);
+    }
 
+    @DeleteMapping("{roomId}/message/{id}")
+    void deleteMessage(@PathVariable Long roomId, @PathVariable Long id) {
+        chatService.deleteMessage(id);
     }
 }
