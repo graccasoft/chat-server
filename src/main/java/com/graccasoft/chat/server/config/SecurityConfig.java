dcf3f31e832675d04ba1b2b9a70d536a0df1b2f6
Gracious Mashasha <graccasoft263@gmail.com>
2024-05-13 12:17:31 +0200

    Fine tune security config

--- src/main/java/com/graccasoft/chat/server/config/SecurityConfig.java
+++ src/main/java/com/graccasoft/chat/server/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.graccasoft.chat.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +17,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v1/chat-room/**").authenticated()
+                        .anyRequest().permitAll()
+                )
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
