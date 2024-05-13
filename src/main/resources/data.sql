f893501e7d4f5173dda74fdb3434660f9205f469
Gracious Mashasha <graccasoft263@gmail.com>
2024-05-13 12:30:32 +0200

    auto increment for IDs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO chat_room(id, name)
 VALUES(1, 'Fishing');
 
 INSERT INTO app_user(id,username, password)
-VALUES(1, 'angler', '$2a$12$6wTMtUZ9z5XAhxx.YNlVIeDRhK.dT/gImfJBAjN7dCxOqYLO71TrO');
+VALUES(10, 'angler', '$2a$12$6wTMtUZ9z5XAhxx.YNlVIeDRhK.dT/gImfJBAjN7dCxOqYLO71TrO');
 
-INSERT INTO chat_message(id,app_user_id, chat_room_id, message, sent_date)
-VALUES(1, 1, 1, 'Where is the cheapest tackle shop', CURRENT_DATE());
+INSERT INTO chat_message(app_user_id, chat_room_id, message, sent_date)
+VALUES( 10, 1, 'Where is the cheapest tackle shop', CURRENT_DATE());
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS chat_room(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS chat_message(
-    id INT NOT NULL,
-    app_user_id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    app_user_id INT NOT NULL AUTO_INCREMENT,
     chat_room_id INT NOT NULL,
     message VARCHAR(100) NOT NULL,
     sent_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS app_user(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50),
     password VARCHAR(100)
 );
