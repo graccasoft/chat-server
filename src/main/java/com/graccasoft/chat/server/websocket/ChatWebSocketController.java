69c8ccdc1b599d38a8571a0d7810149fdfd445c6
Gracious Mashasha <graccasoft263@gmail.com>
2024-05-13 14:08:34 +0200

    websockets endpoint impl

--- src/main/java/com/graccasoft/chat/server/websocket/ChatWebSocketController.java
+++ src/main/java/com/graccasoft/chat/server/websocket/ChatWebSocketController.java
@@ -2,16 +2,24 @@ package com.graccasoft.chat.server.websocket;
 
 import com.graccasoft.chat.server.model.ChatMessageDto;
 import com.graccasoft.chat.server.model.SendMessageDto;
+import com.graccasoft.chat.server.service.ChatService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatWebSocketController {
+
+    private final ChatService chatService;
+
+    public ChatWebSocketController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
     @MessageMapping
     @SendTo("/topic/room-1")
-    public ChatMessageDto greeting(SendMessageDto message) throws Exception {
-        //todo implement sockets
-        return null;
+    public ChatMessageDto greeting(SendMessageDto message, Authentication authentication) throws Exception {
+        return chatService.sendMessage(message, authentication.getName());
     }
 }
