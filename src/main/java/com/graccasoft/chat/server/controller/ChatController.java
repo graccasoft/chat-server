88c4a42ef78630881139d46617a7c7000015f67a
Gracious Mashasha <graccasoft263@gmail.com>
2024-05-13 10:44:09 +0200

    Removed error @controller

--- src/main/java/com/graccasoft/chat/server/controller/ChatController.java
+++ src/main/java/com/graccasoft/chat/server/controller/ChatController.java
@@ -1,13 +1,10 @@
 package com.graccasoft.chat.server.controller;
 
 import com.graccasoft.chat.server.model.ChatMessage;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@Controller
 @RestController
 @RequestMapping("chat-room")
 public class ChatController {
